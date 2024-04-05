30f351c4641ef3b9dc05a5962206259c08bd620d
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-04-05 20:36:47 +0300

    comment for base config added to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,3 +6,9 @@ export default defineConfig({
   plugins: [react()],
   base: 'vite_template',
 });
+
+// base: 'repo_name'
+
+// If you are deploying to https://<USERNAME>.github.io/, or to a custom domain through GitHub Pages (eg. www.example.com), set base to '/'. Alternatively, you can remove base from the configuration, as it defaults to '/'.
+
+// If you are deploying to https://<USERNAME>.github.io/<REPO>/ (eg. your repository is at https://github.com/<USERNAME>/<REPO>), then set base to '/<REPO>/'.
